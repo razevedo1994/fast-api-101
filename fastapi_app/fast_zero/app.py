b7b51c01367222248bb5e8aea7031b50ef0822de
razevedo1994 <razevedo.contato@gmail.com>
2025-04-22 08:52:16 -0300

    Create create user route

--- fastapi_app/fast_zero/app.py
+++ fastapi_app/fast_zero/app.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserSchema
 
 app = FastAPI()
 
@@ -10,3 +10,8 @@ app = FastAPI()
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Hello World"}
+
+
+@app.post("/users/")
+def create_user(user: UserSchema):
+    pass
