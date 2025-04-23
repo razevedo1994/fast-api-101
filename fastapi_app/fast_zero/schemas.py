05099abd3960c90ae86c8865bd26962115dcd990
razevedo1994 <razevedo.contato@gmail.com>
2025-04-23 07:28:23 -0300

    Add schemas

--- fastapi_app/fast_zero/schemas.py
+++ fastapi_app/fast_zero/schemas.py
@@ -9,3 +9,13 @@ class UserSchema(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+
+class UserDB(UserSchema):
+    id: int
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
