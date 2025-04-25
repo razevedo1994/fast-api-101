750f3cd0c171eeafada0a4ba471cece1f1638e17
razevedo1994 <razevedo.contato@gmail.com>
2025-04-25 07:55:57 -0300

    test: Create get users test

--- fastapi_app/tests/test_app.py
+++ fastapi_app/tests/test_app.py
@@ -24,3 +24,18 @@ def test_create_user(client):
         "email": "test@test.com",
         "id": 1,
     }
+
+
+def test_read_users(client):
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "username": "testuser",
+                "email": "test@test.com",
+                "id": 1,
+            }
+        ]
+    }
