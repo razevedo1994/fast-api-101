2d8799cbd30d76f947eae6bf7f72805741b9edd9
razevedo1994 <razevedo.contato@gmail.com>
2025-04-24 22:43:59 -0300

    tests: Add test to create user

--- fastapi_app/tests/test_app.py
+++ fastapi_app/tests/test_app.py
@@ -12,3 +12,23 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello World"}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        "/users/",
+        json={
+            "username": "testuser",
+            "password": "password",
+            "email": "test@test.com",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "testuser",
+        "email": "test@test.com",
+        "id": 1,
+    }
