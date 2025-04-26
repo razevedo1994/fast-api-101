2bcac2b246739e43ab7c280df3678389f69cdd52
razevedo1994 <razevedo.contato@gmail.com>
2025-04-26 15:48:15 -0300

    test: Create update user test

--- fastapi_app/tests/test_app.py
+++ fastapi_app/tests/test_app.py
@@ -39,3 +39,21 @@ def test_read_users(client):
             }
         ]
     }
+
+
+def test_update_user(client):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "joao",
+            "email": "joao@example.com",
+            "password": "mypassword",
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "joao",
+        "email": "joao@example.com",
+        "id": 1,
+    }
